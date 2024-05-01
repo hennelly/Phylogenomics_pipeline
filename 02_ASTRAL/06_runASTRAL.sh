faa4aefc9d2eff851ef71f183dbda33fa2c3d733
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-30 23:10:13 -0400

    Update 06_runASTRAL.sh

--- 02_ASTRAL/06_runASTRAL.sh
+++ 02_ASTRAL/06_runASTRAL.sh
@@ -6,7 +6,7 @@
 #SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/astral_auto_byspecies_atry2ll_feb9_annotate.out
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/astral_auto_byspecies_atry2ll_feb9_annotate.err
 
-ls *.treefiles > listoftreefiles.txt #get a list of the 5000 trees
+cat *.treefiles > listoftreefiles.txt #get a list of the 5000 trees
 
 java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar --branch-annotate 1 -i listoftreefiles.txt -o full_Feb9_try2_autosomes_branchannotate.tre
 
