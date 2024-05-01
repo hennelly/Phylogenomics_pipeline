acd21977c89132b85bfd3c2a01981ba8c28724b9
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-30 22:22:13 -0400

    Create 01_selectrandomregions.sh

---
+++ 02_ASTRAL/01_selectrandomregions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=astral
+#SBATCH -c 1
+#SBATCH --time 1:10:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.out
+#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.err
+
+GENOME=/projects/mjolnir1/people/crq857/Chapter2/ref/canFam31.fasta.fai
+
+module load bedtools 
+
+bedtools random -g ${GENOME} -l 20000 -n 5000 
+
+#This script is using the dog reference genome to select 5000 random regions that are 20kb in length. 
